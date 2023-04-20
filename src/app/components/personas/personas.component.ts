e0515d5af62446cb4c243559c98ec5123d3f9d64
Itziar Mensa <itziar.mensa@estudiantat.upc.edu>
2023-04-20 09:47:29 +0200

    feat: obtained list users

--- src/app/components/personas/personas.component.ts
+++ src/app/components/personas/personas.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Persona } from 'src/app/models/persona';
+import { DialogService } from 'src/app/services/dialog-service.service';
+import { PersonaService } from 'src/app/services/personas.service';
 
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
   styleUrls: ['./personas.component.css']
 })
-export class PersonasComponent {
+export class PersonasComponent implements OnInit{
+
+  currentPage: number = 1;
+  totalPages: number = 1; 
+  limit: number = 2; 
+
+  constructor( private _personaService:PersonaService, private _router: Router, private dialogService: DialogService) { }
+
+  ngOnInit(): void {
+    this.getPersonas();
+  }
+
+  personas: Persona [] = [];
+
+  getPersonas(){
+    this._personaService.getPersonas(this.currentPage, this.limit).subscribe(data => {
+      this.personas = data.personas;
+      this.totalPages = data.totalPages;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  prevPage() {
+    this.currentPage = this.currentPage - 1; 
+    this.getPersonas();
+  }
+
+  nextPage() {
+    this.currentPage = this.currentPage + 1; 
+    this.getPersonas();
+  }
 
 }
